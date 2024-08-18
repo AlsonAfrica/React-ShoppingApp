91d5a8828d6c610f935dacadfcd4f81f83b40f52
Nhlakanipho <nhlakaniphoradebe337@gmail.com>
2024-08-18 09:42:57 -1100

    react-icons installed, logophoto imported and homePage interface completed not yet functional

--- react-shoppingapp/src/components/HomePage.jsx
+++ react-shoppingapp/src/components/HomePage.jsx
@@ -3,6 +3,8 @@ import "../components/styles/HomePage.css"
 import NavbarHome from "./Navbar-Home";
 import EditableTextField from "./Editable-field";
 import Dropdown from "./drop-down";
+import { GrAdd } from "react-icons/gr";
+import SearchBar from "./mini-searchbar";
 // import SearchBar from "./mini-searchbar";
 
 
@@ -13,19 +15,23 @@ const HomePage = () => (
             <div className="home-text"><h1>Your Shopping Lists</h1>
                 <p>Manage all your shopping lists in one place,Create new lists, add items, and share with friends andd family</p>
             </div>
-            <div className="home-button"><button>Create New List +</button></div>
+            <div className="btn-search">
+                 <div className="home-button"><button>Create New List +</button></div>
+                 <div className="search-bar"><SearchBar/></div>
+            </div>
+           
         </div>
         <div className="lists">
             <div className="my-shopping-lists">
                 <div className="list-container1">
-                    <div className="textfield"><EditableTextField/><Dropdown/></div>
+                    <div className="textfield"><EditableTextField/><Dropdown/><button className="floating-btn1" title="Add-list"><GrAdd /></button></div>
                     <div></div>
-                    <div className="floating-btn"></div>
+                    {/* <button className="floating-btn1"><GrAdd /></button> */}
                 </div>
                 <div className="list-container2">
-                    <div className="textfield"><EditableTextField/><Dropdown/></div>
+                    <div className="textfield"><EditableTextField/><Dropdown/><button className="floating-btn1" title="Add-list"><GrAdd /></button></div>
                     <div></div>
-                    <div className="floating-btn"></div>
+                    
                 </div>
                 {/* <div className="tags"><h1>My Shopping lists <Dropdown/> <SearchBar/></h1></div> */}
                 {/* <div> */}
@@ -36,14 +42,14 @@ const HomePage = () => (
             </div>
             <div className="shared-lists">
                 <div className="list-container3">
-                <div className="textfield"><EditableTextField/><Dropdown/></div>
+                <div className="textfield"><EditableTextField/><Dropdown/><button className="floating-btn1" title="Add-list"><GrAdd /></button></div>
                 <div></div>
-                <div className="floating-btn"></div>
+               
                 </div>
                 <div className="list-container4">
-                <div className="textfield"><EditableTextField/><Dropdown/></div>
+                <div className="textfield"><EditableTextField/><Dropdown/><button className="floating-btn1" title="Add-list"><GrAdd /></button></div>
                 <div></div>
-                <div className="floating-btn"></div>
+                
                 </div>
             </div>
             <div className="sent-list">
